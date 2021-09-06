eeb415bcb9518ee520dea5c81fcc86224b249a4e
Maryna Maksymchuk <risaranma@gmail.com>
2021-09-06 22:02:35 +0300

    slider section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(function(){
-
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-prev.svg" alt="">',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-next.svg" alt="">',
+    });
 });
 
